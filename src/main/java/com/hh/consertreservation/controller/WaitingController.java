71e6da362dde81868006e09d6b895f2d1018e9e9
wonuk(du_ck) <wonuk007@naver.com>
2024-07-12 08:04:59 +0900

    swagger 적용 및 설명 작성 @EnableScheduling 주석해제

--- src/main/java/com/hh/consertreservation/controller/WaitingController.java
+++ src/main/java/com/hh/consertreservation/controller/WaitingController.java
@@ -5,6 +5,7 @@ import com.hh.consertreservation.application.scheduler.WaitingScheduler;
 import com.hh.consertreservation.controller.dto.TokenIssued;
 import com.hh.consertreservation.controller.dto.data.ResponseData;
 import com.hh.consertreservation.domain.dto.Token;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ public class WaitingController {
     private final TokenFacade tokenFacade;
 
     @PostMapping("/issued")
+    @Operation(summary = "대기열 토큰 발급", description = "대기열 토큰을 발급한다.")
     public ResponseEntity<ResponseData> tokenIssued(@RequestBody TokenIssued.Request req) throws Exception {
 
         Optional<Token> token = tokenFacade.issued(req.getUserId(), WaitingScheduler.MAXIMUM_ONGOING_COUNT);
@@ -41,6 +43,7 @@ public class WaitingController {
      * @return
      */
     @GetMapping("/waiting")
+    @Operation(summary = "대기열 순번 조회", description = "다음 OnGoing 우선순위의 토큰id를 조회한다")
     public ResponseEntity<ResponseData> waitingCount() {
         long nextOnGoingUserId = WaitingScheduler.nextOnGoingTokenId;
 
